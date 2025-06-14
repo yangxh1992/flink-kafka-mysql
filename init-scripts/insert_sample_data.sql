1a284bc848d9e62e03a13612ed23b420fb48b09b
杨兴辉 <yangxh1992@gmail.com>
2025-06-14 12:09:56 +0800

    flink从kafka读取数据写入mysql

--- init-scripts/insert_sample_data.sql
+++
@@ -1,9 +0,0 @@
-USE
-flink_data;
-
-INSERT INTO sensor_data (sensor_id, temperature, timestamp_ms)
-VALUES ('sensor_A', 20.1, 1678887000000),
-       ('sensor_B', 22.5, 1678887001000),
-       ('sensor_C', 21.0, 1678887002000),
-       ('sensor_A', 20.8, 1678887003000),
-       ('sensor_B', 23.0, 1678887004000);
